ea20c454abb0fa8d873c054cd22a941a18ec3333
Talib Hussain Naseri <talib.hussain18@vit.edu>
2022-11-28 13:11:45 +0530

    First setup

---
+++ src/charts/main_page.js
@@ -0,0 +1,7 @@
+export const MainPageView = (props) => {
+  return (
+    <div className="container m-4 text-center border">
+      <h1>Main Page Diagram Ideas</h1>
+    </div>
+  )
+}
---
+++ src/charts/user_page.js
@@ -0,0 +1,7 @@
+export const UserPageView = (props) => {
+  return (
+    <div className="container m-4 text-center border">
+      <h1>User Page Diagram Ideas</h1>
+    </div>
+  )
+}
